337320f59ecc1ac739da627334c13904c349c9cc
mark mwendwa <mwendwamark310@gmail.com>
2023-06-20 11:36:28 +0300

    completed code along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,31 @@ class BirdsController < ApplicationController
     end
   end
 
+  #adding an update action
+  def update
+    #We first find the bird by Id 
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      #Then we update it using the .update() method
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found " }, status: :not_found
+    end
+  end
+
+  def increment_likes 
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      brid.update(likes: bird.likes + 1)
+      render json: bird 
+    else 
+      render json: { error: "Bird not found"},status: :not_found
+  end 
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
-
 end
